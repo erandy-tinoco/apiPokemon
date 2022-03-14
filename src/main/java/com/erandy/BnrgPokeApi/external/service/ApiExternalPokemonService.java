86eabeebc095573cf91c65765a8a8b7fef5a4cc9
Erandy Tinoco <tinoco.erandy@gmail.com>
2022-03-14 16:11:25 -0600

    Https

--- src/main/java/com/erandy/BnrgPokeApi/external/service/ApiExternalPokemonService.java
+++ src/main/java/com/erandy/BnrgPokeApi/external/service/ApiExternalPokemonService.java
@@ -8,10 +8,12 @@ import com.erandy.BnrgPokeApi.external.dto.RaizAbility;
 import com.erandy.BnrgPokeApi.external.dto.Abilities;
 import com.erandy.BnrgPokeApi.external.dto.Ability;
 import com.google.gson.Gson;
+import java.util.Arrays;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
@@ -28,6 +30,10 @@ public class ApiExternalPokemonService {
 
         String url = "https://pokeapi.co/api/v2/pokemon/" + nombre;
         HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        headers.add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36");
+        //HttpEntity<String> entity = new HttpEntity<String>("parameters", headers);
+
         RestTemplate restTemplate = new RestTemplate();
 
         HttpEntity<Void> request = new HttpEntity<>(headers);
